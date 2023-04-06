6cf540aedb5ddd373b477452bdf82bab01863c2b
Crowdbotics <engineering@crowdbotics.com>
2023-04-06 20:26:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dawn_heart_dev_75001/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dawn_heart_dev_75001;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dawn_heart_dev_75001";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-heart-dev-75001",
+  url: "https://dawn-heart-dev-75001.botics.co",
+  api: "https://dawn-heart-dev-75001.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
